7f83e8f3014922e6fe7322608434bf9609899c48
hucnghi30 <xomdangvaisf@gmail.com>
2025-05-06 08:34:19 +0700

    finish login for student and teacher [api, AuthController, AuthMiddleware, AuthRepository, AuthService]

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+
+use App\Http\Controllers\AuthController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::group(['prefix'=>'course'],function(){
     Route::get('/getByStudentId/{id}',[CourseController::class,'getCourseByStudentId']);
 });
+
+
+use App\Http\Middleware\AuthMiddleware;
+
+Route::post('/login', [AuthController::class, 'login'])->middleware(AuthMiddleware::class);
