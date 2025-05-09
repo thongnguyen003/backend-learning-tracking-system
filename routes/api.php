1a99d57c565622a79705d0fa1deb3de8768f0b51
bnuoch thi thuy <bnuochthithuy13032005@gmail.com>
2025-05-09 08:55:41 +0700

    Done develop the student information view feature

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\Api\StudentProfileController;
+use App\Http\Controllers\StudentController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::group(['prefix'=>'course'],function(){
     Route::get('/getByStudentId/{id}',[CourseController::class,'getCourseByStudentId']);
 });
+Route::apiResource('students', StudentProfileController::class);
+
+Route::middleware('auth:api')->post('/student/change-password', [StudentController::class, 'changePassword']);
