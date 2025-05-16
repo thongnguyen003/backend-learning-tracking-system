8d48191b6e79001a4fcb930ea37edeacee96f126
bnuoch thi thuy <bnuochthithuy13032005@gmail.com>
2025-05-16 14:06:06 +0700

    feature/17 - create JournalClassesService

--- app/Services/JournalClassesService.php
+++ app/Services/JournalClassesService.php
@@ -6,21 +6,25 @@
         public function __construct(JournalClassesRepository $reponsitory){
             $this->repository = $reponsitory;
         }
+
         public function getAll(){
             return $this->repository->getAll();
         }
+        
         public function getById($id){
             return $this->repository->findById($id);
         }
+
         public function create(array $data){
             return $this->repository->create($data);
         }
+
         public function update($id, array $data){
             return $this->repository->update($id,$data);
         }
+
         public function delete($id){
             return $this->repository->delete($id);
         }
     }
 ?>
-
