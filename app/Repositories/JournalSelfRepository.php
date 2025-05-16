73f054b06b6396d45726e1dec605b11741f92415
bnuoch thi thuy <bnuochthithuy13032005@gmail.com>
2025-05-16 14:04:58 +0700

    feature/17 - create JournalSelfRepositories

--- app/Repositories/JournalSelfRepository.php
+++ app/Repositories/JournalSelfRepository.php
@@ -1,40 +1,28 @@
 <?php
 
-
 namespace App\Repositories;
-
-
 use App\Models\JournalSelf;
-
-
 class JournalSelfRepository
 {
     protected $model;
-
-
     public function __construct(JournalSelf $journalSelf)
     {
         $this->model = $journalSelf;
     }
 
-
     public function getAll()
     {
         return $this->model->all();
     }
-
-
     public function findById($id)
     {
         return $this->model->find($id);
     }
 
-
     public function create(array $data)
     {
         return $this->model->create($data);
     }
-
 
     public function update($id, array $data)
     {
