f7e5e7ab30480d4c37d5f55a2fa1e1041ac6d73f
bnuoch thi thuy <bnuochthithuy13032005@gmail.com>
2025-05-25 11:38:20 +0700

    feature/30 - update classes Model

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -4,12 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use App\Models\ClassTeacher;
+use App\Models\Teacher;
 
 class Classes extends Model
 {
     use HasFactory; 
     protected $table="classes";
+
+       protected $fillable = [
+        'name',
+        'quantity',
+        'start_day',
+        'end_day',
+        'status',
+    ];
+
+    public function teachers(){
+        return $this->belongsToMany(Teacher::class, 'class_teachers')
+        ->using(ClassTeacher::class)
+        ->withTimestamps();
+    }
+
 
     public function class_teachers (){
         return $this->hasMany(ClassTeacher::class,'classes_id');
