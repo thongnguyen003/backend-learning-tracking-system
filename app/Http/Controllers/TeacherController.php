b2ad1c5164c0937d4305ebdee093413b6b8ab9b0
bnuoch thi thuy <bnuochthithuy13032005@gmail.com>
2025-05-25 11:36:14 +0700

    feature/30 - done create claas_teacher controller

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Services\TeacherService;
+
+
+
+
+class TeacherController extends Controller
+{
+    protected $teacherService;
+    public function show(int $id)
+    {
+        $teacher = $this->teacherService->findById($id);
+        return response()->json($teacher);
+    }
+    public function __construct(TeacherService $teacherService){
+        $this->teacherService = $teacherService;
+    }
+}
